8724e703332a442f2e4c9d7689cabcd7418166e6
Jaime Lopez-Merizalde <jaime.lopez@petdesk.com>
2025-05-19 00:23:34 -0400

    lesson 12 on ridge regression complete. changes to lesson 11 and library for hosting new additions to ridge regression

--- Library/data.py
+++ Library/data.py
@@ -15,7 +15,32 @@ def get_data(filename, rename_columns={}, remap={}, combinator={}, **kwargs):
 
     return df
 
-def missing_values(df: pd.DataFrame) -> pd.DataFrame:
-    df_clean = df.copy(deep=True)
+def remove_missing_and_nonNumerical_values(X, y):
+    """
+    Removes missing and non-numerical values from dataframe 'X' and corresponding reponse variable y. 
+    Prints minor statistics such as original shapes, categorical variables removed, and new sizes.
 
-    return df_clean
+    Parameters
+    ------
+    X (pd.DataFrame): Original dataframe. Numerical and non-numerical datatypes must be pre-assigned as 
+    numerical type and category.
+    y (pd.DataFrame): Original response variable.
+
+    Returns
+    ------
+    X (pd.DataFrame): Reduced dataframe with non nans or categorical column variables.
+    y (pd.DataFrame): Reduced response variable matching corresponding rows of reduced dataframe 'X'.
+    """
+    print(f"Original Size X: {X.shape} y: {y.shape}")
+
+    # Drop Categorical Values. Nonnumerical Dtypes must be 
+    categorical_columns = X.dtypes[X.dtypes=="category"].index.values
+    X = X.drop(columns=categorical_columns)
+    print("Removed {}".format(categorical_columns))
+
+    # Drop Missing Values.
+    X = X.dropna()
+    # Make sure you reduce y as well, since the above is a row-reduction technique.
+    y = y[X.index]
+    print(f"New Size X: {X.shape} y: {y.shape}")
+    return X, y
